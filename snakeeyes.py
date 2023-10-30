82313a5fd2fb751ac343dce8fccb7c74e523050b
Bart Massey <bart.massey@gmail.com>
2023-10-30 14:37:31 -0700

    fixed snakeeyes

--- snakeeyes.py
+++ snakeeyes.py
@@ -5,12 +5,9 @@ def die():
     return random.randrange(1, 7)
 
 def twodice():
-    while True:
-        d1 = die()
-        d2 = die()
-        if d1 == d2 and d1 != 1:
-            continue
-        return (d1, d2)
+    d1 = die()
+    d2 = die()
+    return (d1, d2)
 
 def ps(n):
     rs = 0
